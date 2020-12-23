7cadc8de465b2ff2c9c2f00f306ca1d6dd5daae6
GiGGioSo <gioele.pettarini@gmail.com>
2020-12-23 19:05:31 +0100

    Initial commit of a random project

---
+++ RandomProgram/src/MainClass.java
@@ -0,0 +1,19 @@
+
+public class MainClass {
+	
+	static Point p1;
+	
+	public static void main(String[] args) {
+		initialize();
+		getInfo();
+	}
+	
+	public static void initialize() {
+		p1 = new Point(4, 5);
+	}
+	
+	public static void getInfo() {
+		System.out.println("Point 1: " + p1.toString());
+	}
+
+}
